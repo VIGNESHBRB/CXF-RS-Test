625798d2fb3a07c91d0b223531d09a442b8146c6
VIGNESH BABU B R B <vigneshbabu.brb@cognizant.com>
2016-10-19 18:46:21 +0530

    Asynchronous WebService POC

--- brb.test.cxfrs/src/main/java/brb/research/rest/Service.java
+++ brb.test.cxfrs/src/main/java/brb/research/rest/Service.java
@@ -1,9 +1,14 @@
 package brb.research.rest;
 
+import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.container.AsyncResponse;
+import javax.ws.rs.container.Suspended;
 import javax.ws.rs.core.MediaType;
+
 
 /**
  * 
@@ -22,4 +27,29 @@ public class Service {
 	public String test() {
 		return "hello";
 	}
+
+	@GET
+	@Consumes("application/json")
+	@Produces("application/json")
+	@Path("/{name}")
+	public void submit(@PathParam(value = "name") final String name, final @Suspended AsyncResponse response) {
+		System.out.println(Thread.currentThread().getName());
+		new Thread() {
+			public void run() {
+				System.out.println(Thread.currentThread().getName());
+				
+				System.out.println("Received input...");
+				try {
+					Thread.sleep(5000);
+					
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				//response.resume("Hello " + name);
+			}
+		}.start();
+		
+		
+	}
+
 }
